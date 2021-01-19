44a2f6f5c94b3610d096093fcf0f684c64464167
LeisyVasquez <Leisyyvalentina@gmail.com>
2021-01-18 23:03:33 -0500

    Estudio de comandos generales

---
+++ MySQL/generales.sql
@@ -0,0 +1,4 @@
+-- Comando de conexión a tráves de consola:  mysql -h localhost -u root 
+SHOW DATABASES; -- Visualizar las bases de datos en mysql.
+SHOW TABLES; -- Visualizar las tablas en la bases de datos.
+USE BD1; --Indica a que BD se quiere reedireccionar
